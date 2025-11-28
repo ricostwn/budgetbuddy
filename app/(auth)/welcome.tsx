3fc86b151963936bea5d53f49fd6eed75e64f7bd
nathanaelrico <nathanaelricosetiawan@Nathanaels-MacBook-Air.local>
2025-11-28 12:10:50 +0700

    landing page dan splash screen

---
+++ app/(auth)/welcome.tsx
@@ -0,0 +1,98 @@
+import Button from '@/components/Button';
+import ScreenWrapper from '@/components/ScreenWrapper';
+import Typo from '@/components/Typo';
+import { colors, spacingX, spacingY } from '@/constants/theme';
+import { verticalScale } from '@/utils/styling';
+import React from 'react';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import Animated, { FadeIn } from 'react-native-reanimated';
+
+const Welcome: React.FC = () => {
+    return (
+        <ScreenWrapper>
+            <View style={styles.container}>
+                {/* login button and image*/}
+                <View>
+                    <TouchableOpacity style={styles.loginButton}>
+                        <Typo fontWeight={'500'}>Sign In</Typo>
+                    </TouchableOpacity>
+
+                    <Animated.Image
+                        entering={FadeIn.duration(500)}
+                        source={require('@/assets/images/welcome.png')}
+                        style={styles.welcomeImage}
+                        resizeMode="contain"
+                    />
+                </View>
+
+                {/* footer */}
+                <View style={styles.footer}>
+                    <View style={{ alignItems: 'center' }}>
+                        <Typo size={30} fontWeight={'800'}>
+                            Always take control
+                        </Typo>
+                        <Typo size={30} fontWeight={'800'}>
+                            of your finances
+                        </Typo>
+                    </View>
+
+                    <View style={{ alignItems: 'center' }}>
+                        <Typo size={17} color={colors.textLight}>
+                            Finances must be arranged to set a better
+                        </Typo>
+                        <Typo size={17} color={colors.textLight}>
+                            lifestyle in future
+                        </Typo>
+                    </View>
+
+                    <View style={styles.buttonContainer}>
+                        <Button>
+                            <Typo size={22} color={colors.neutral900} fontWeight={'600'}>
+                                Get Started
+                            </Typo>
+                        </Button>
+                    </View>
+                </View>
+            </View>
+        </ScreenWrapper>
+    );
+};
+
+export default Welcome;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'space-between',
+        paddingTop: spacingY._7,
+    },
+    welcomeImage: {
+        height: verticalScale(300),
+        width: '100%',
+        alignSelf: 'center',
+        marginTop: verticalScale(100),
+    },
+    loginButton: {
+        alignSelf: 'flex-end',
+        marginRight: spacingX._20,
+    },
+    footer: {
+        backgroundColor: colors.neutral900,
+        alignItems: 'center',
+        paddingTop: verticalScale(30),
+        paddingBottom: verticalScale(45),
+        gap: spacingY._20,
+        shadowColor: 'white',
+        shadowOffset: {
+            width: 0,
+            height: -10,
+        },
+        elevation: 10,
+        shadowOpacity: 0.15,
+        shadowRadius: 25,
+    },
+    buttonContainer: {
+        width: '100%',
+        paddingHorizontal: spacingX._25,
+    },
+});
