bd1cc3c2e3b7e93c0cb0b989b93952f865757041
nathanaelrico <nathanaelricosetiawan@Nathanaels-MacBook-Air.local>
2025-12-04 21:03:03 +0700

    statistik

--- services/transactionService.ts
+++ services/transactionService.ts
@@ -1,6 +1,9 @@
 import { firestore } from "@/config/firebase"
+import { colors } from "@/constants/theme"
 import { ResponseType, TransactionType, WalletType } from "@/types"
-import { collection, deleteDoc, doc, getDoc, setDoc, updateDoc } from "firebase/firestore"
+import { getLast12Months, getLast7Days, getYearsRange } from "@/utils/common"
+import { scale } from "@/utils/styling"
+import { collection, deleteDoc, doc, getDoc, getDocs, orderBy, query, setDoc, Timestamp, updateDoc, where } from "firebase/firestore"
 import { uploadFileToCloudinary } from "./imageService"
 import { createOrUpdateWallet } from "./walletService"
 
@@ -231,7 +234,7 @@ export const deleteTransaction = async (
         const newWalletAmount =
             walletData?.amount! - 
             (transactionType == 'income' ? transactionAmount : -transactionAmount)
-            
+
         const newIncomeExpenseAmount =
             walletData[updateType]! - transactionAmount!
 
@@ -252,5 +255,219 @@ export const deleteTransaction = async (
     } catch (err: any) {
         console.log('error updating wallet for new transaction: ', err)
         return { success: false, msg: err.message }
+    }
+}
+
+export const fetchWeeklyStats = async (
+    uid: string
+): Promise<ResponseType> => {
+    try {
+        const db = firestore
+        const today = new Date()
+        const sevenDaysAgo = new Date(today)
+        sevenDaysAgo.setDate(today.getDate() - 7)
+
+        const transactionsQuery = query(
+            collection(db, 'transactions'),
+            where('date', '>=', Timestamp.fromDate(sevenDaysAgo)),
+            where('date', '<=', Timestamp.fromDate(today)),
+            orderBy('date', 'desc'),
+            where('uid', '==', uid)
+        )
+
+        const querySnapshot = await getDocs(transactionsQuery)
+        const weeklyData = getLast7Days()
+        const transactions: TransactionType[] = []
+
+        //mapping each transaction in day
+        querySnapshot.forEach((doc) => {
+            const transaction = doc.data() as TransactionType
+            transaction.id = doc.id
+            transactions.push(transaction)
+
+            const transactiondate = (transaction.date as Timestamp)
+                .toDate()
+                .toISOString()
+                .split('T')[0] //as spesific dateon, Tue
+            
+            const dayData = weeklyData.find((day => day.date === transactiondate)
+            )
+            if (dayData) {
+                if (transaction.type === 'income') {
+                    dayData.income += Number(transaction.amount)
+                } else if (transaction.type === 'expense') {
+                    dayData.expense += Number(transaction.amount)
+                }
+            }
+        })
+
+        const stats = weeklyData.flatMap((day) => [
+            {
+                value: day.income,
+                label: day.day,
+                spacing: scale(4),
+                labelWidth: scale(30),
+                frontColor: colors.primary,
+            },
+            {value: day.expense, frontColor: colors.rose,}
+        ])
+
+        return {
+            success: true,
+            data: {
+                stats,
+                transactions
+            }
+        }
+
+    } catch (err: any) {
+        console.log('error fetching weekly stats: ', err)
+        return { success: false, msg: err.message }
+    }
+}
+
+export const fetchMonthlyStats = async (
+    uid: string
+): Promise<ResponseType> => {
+    try {
+        const db = firestore
+        const today = new Date()
+        const twelveMonthsAgo = new Date(today)
+        twelveMonthsAgo.setDate(today.getMonth() - 12)
+
+        //define query to fetch transactions in last 12 months
+        const transactionsQuery = query(
+            collection(db, 'transactions'),
+            where('date', '>=', Timestamp.fromDate(twelveMonthsAgo)),
+            where('date', '<=', Timestamp.fromDate(today)),
+            orderBy('date', 'desc'),
+            where('uid', '==', uid)
+        )
+
+        const querySnapshot = await getDocs(transactionsQuery)
+        const monthlyData = getLast12Months()
+        const transactions: TransactionType[] = []
+
+        //process transactions to calculate income and expense per month
+        querySnapshot.forEach((doc) => {
+            const transaction = doc.data() as TransactionType
+            transaction.id = doc.id //include document ID in transaction data
+            transactions.push(transaction)
+
+            const transactionDate = (transaction.date as Timestamp).toDate()
+            
+            const monthName = transactionDate.toLocaleString('default', { month: 'short' })
+            const shortYear = transactionDate.getFullYear().toString().slice(-2)
+            const monthData = monthlyData.find(
+                (month) => month.month === `${monthName} ${shortYear}`
+            )
+
+            
+            if (monthData) {
+                if (transaction.type === 'income') {
+                    monthData.income += transaction.amount
+                } else if (transaction.type === 'expense') {
+                    monthData.expense += transaction.amount
+                }
+            }
+        })
+
+        // reformat monthlydata for the bar chart with income and expense entries for each month
+
+        const stats = monthlyData.flatMap((month) => [
+            {
+                value: month.income,
+                label: month.month,
+                spacing: scale(4),
+                labelWidth: scale(46),
+                frontColor: colors.primary,
+            },
+            {value: month.expense, frontColor: colors.rose,}
+        ])
+
+        return {
+            success: true,
+            data: {
+                stats,
+                transactions
+            }
+        }
+
+    } catch (error: any) {
+        console.log('error fetching monthly transactions: ', error)
+        return { success: false, msg: 'failed to fetch monthly transactions' }
+    }
+}
+
+export const fetchYearlyStats = async (
+    uid: string
+): Promise<ResponseType> => {
+    try {
+        const db = firestore
+
+        const transactionsQuery = query(
+            collection(db, 'transactions'),
+            orderBy('date', 'desc'),
+            where('uid', '==', uid)
+        )
+
+        const querySnapshot = await getDocs(transactionsQuery)
+        const transactions: TransactionType[] = []
+
+        const firstTransaction = querySnapshot.docs.reduce((earliest, doc)=>{
+            const transactionDate = doc.data().date.toDate()
+            return transactionDate < earliest ? transactionDate : earliest
+        }, new Date())
+
+        const firstYear = firstTransaction.getFullYear()
+        const currentYear = new Date().getFullYear()
+        const yearlyData = getYearsRange(firstYear, currentYear)
+
+        //process transactions to calculate income and expense per month
+        querySnapshot.forEach((doc) => {
+            const transaction = doc.data() as TransactionType
+            transaction.id = doc.id //include document ID in transaction data
+            transactions.push(transaction)
+
+            const transactionYear = (transaction.date as Timestamp).toDate().getFullYear()
+            
+            const yearData = yearlyData.find(
+                (item:any) => item.year === transactionYear.toString()
+            )
+
+            
+            if (yearData) {
+                if (transaction.type === 'income') {
+                    yearData.income += transaction.amount
+                } else if (transaction.type === 'expense') {
+                    yearData.expense += transaction.amount
+                }
+            }
+        })
+
+        // reformat monthlydata for the bar chart with income and expense entries for each month
+
+        const stats = yearlyData.flatMap((year: any) => [
+            {
+                value: year.income,
+                label: year.year,
+                spacing: scale(4),
+                labelWidth: scale(35),
+                frontColor: colors.primary,
+            },
+            {value: year.expense, frontColor: colors.rose,}
+        ])
+
+        return {
+            success: true,
+            data: {
+                stats,
+                transactions
+            }
+        }
+
+    } catch (error: any) {
+        console.log('error fetching yearly transactions: ', error)
+        return { success: false, msg: 'failed to fetch yearly transactions' }
     }
 }
